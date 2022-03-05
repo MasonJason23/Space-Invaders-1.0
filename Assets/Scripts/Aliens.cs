6df9cec78a380a34c711172cb104d843826a937e
Jason Casareno <jcasareno@csumb.edu>
2022-03-04 22:21:50 -0800

    Implemented enemy block movement with delay and row advancement

--- Assets/Scripts/Aliens.cs
+++ Assets/Scripts/Aliens.cs
@@ -8,7 +8,13 @@ public class Aliens : MonoBehaviour
     public Enemy[] prefabs;
     public int row = 5;
     public int col = 11;
-
+    public float movementSpd = 1;
+    public float movementDelay = 1;
+    public float rowAdvanceY = 1;
+    public Vector3 direction = Vector3.right;
+    
+    private float accumulatedTime = 0f;
+    private int totalAliens = 0;
     private void Awake()
     {
         for (int i = 0; i < row; i++)
@@ -19,19 +25,34 @@ public class Aliens : MonoBehaviour
                 float newX = alien.transform.position.x + j;
                 float newY = alien.transform.position.y + i;
                 alien.transform.position = new Vector2(newX, newY);
+                totalAliens++;
             }
         }
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        accumulatedTime += Time.deltaTime;
+        if (accumulatedTime > movementDelay)
+        {
+            this.transform.position += (direction * movementSpd);
+            accumulatedTime = 0f;
+        }
+
+        foreach (Transform alien in this.transform)
+        {
+            if (!alien.gameObject.activeInHierarchy)
+            {
+                continue;
+            }
+            else if (Math.Abs(alien.transform.position.x) > 11f)
+            {
+                this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y - rowAdvanceY, 0f);
+                direction.x *= -1;
+                this.transform.position += (direction * movementSpd);
+                break;
+            }
+        }
     }
 }
